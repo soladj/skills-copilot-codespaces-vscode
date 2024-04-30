55439bbfe4bc3e36f5d0273136c0839665d08722
Álvaro Sola <alvaro.sola-soria@capgemini.com>
2024-04-30 11:16:42 +0200

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'React', 'Node', 'Python', 'Django', 'Ruby', 'Ruby on Rails'],
+    getSkills: function() {
+      return this.skills;
+    }
+  }
+}
